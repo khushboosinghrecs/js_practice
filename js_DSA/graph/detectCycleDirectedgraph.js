8b43a63bb8b7ffc58640f42788da487dcc3d1c42
Khushboo Singh <9918khush@gmail.com>
2024-06-05 00:12:00 +0530

    graph

--- js_DSA/graph/detectCycleDirectedgraph.js
+++ js_DSA/graph/detectCycleDirectedgraph.js
@@ -1,43 +1,105 @@
-class Graph{
-    constructor(vertices){
+class UndirectedGraph {
+    constructor(vertices) {
         this.vertices = vertices;
         this.adjList = new Map();
     }
 
-    addVertex(v){
-        this.adjList.set(v, [])
+    addVertex(v) {
+        this.adjList.set(v, []);
     }
 
-    addEdge(u, v){
+    addEdge(u, v) {
         this.adjList.get(u).push(v);
+        this.adjList.get(v).push(u); // Because the graph is undirected
     }
 
-    detectCycle(){
+    detectCycle() {
         const visited = new Set();
-        const recStack = new Set();
 
-        for(let vertex of this.adjList.keys()){
-            if(this.detectCycleUtill(vertex, visited, recStack)){
-                return true;
+        for (let vertex of this.adjList.keys()) {
+            if (!visited.has(vertex)) {
+                if (this.detectCycleUtil(vertex, visited, -1)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    detectCycleUtill(v, visited, recStack){
-        if(!visited[v]){
+    detectCycleUtil(v, visited, parent) {
+        visited.add(v);
 
+        const neighbors = this.adjList.get(v);
+        for (let neighbor of neighbors) {
+            if (!visited.has(neighbor)) {
+                if (this.detectCycleUtil(neighbor, visited, v)) {
+                    return true;
+                }
+            } else if (neighbor !== parent) {
+                return true; // There is a cycle
+            }
+        }
+
+        return false;
+    }
+}
+
+// Example usage for undirected graph:
+const ug = new UndirectedGraph(4);
+ug.addVertex(0);
+ug.addVertex(1);
+ug.addVertex(2);
+ug.addVertex(3);
+ug.addEdge(0, 1);
+ug.addEdge(1, 2);
+ug.addEdge(2, 0);
+ug.addEdge(2, 3);
+
+if (ug.detectCycle()) {
+  console.log("Undirected graph contains cycle");
+} else {
+  console.log("Undirected graph does not contain cycle");
+}
+
+class DirectedGraph {
+    constructor(vertices) {
+        this.vertices = vertices;
+        this.adjList = new Map();
+    }
+
+    addVertex(v) {
+        this.adjList.set(v, []);
+    }
+
+    addEdge(u, v) {
+        this.adjList.get(u).push(v);
+    }
+
+    detectCycle() {
+        const visited = new Set();
+        const recStack = new Set();
+
+        for (let vertex of this.adjList.keys()) {
+            if (!visited.has(vertex)) {
+                if (this.detectCycleUtil(vertex, visited, recStack)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    detectCycleUtil(v, visited, recStack) {
+        if (!visited.has(v)) {
             visited.add(v);
             recStack.add(v);
 
             const neighbors = this.adjList.get(v);
-
-            for(let neighbor of neighbors){
-                if(!visited[neighbor] && this.detectCycleUtill(neighbor, visited, recStack) ){
+            for (let neighbor of neighbors) {
+                if (!visited.has(neighbor) && this.detectCycleUtil(neighbor, visited, recStack)) {
                     return true;
-                }
-                else if(recStack.has(neighbor)){
-                    return true;
+                } else if (recStack.has(neighbor)) {
+                    return true; // There is a cycle
                 }
             }
         }
@@ -47,20 +109,19 @@ class Graph{
     }
 }
 
+// Example usage for directed graph:
+const dg = new DirectedGraph(4);
+dg.addVertex(0);
+dg.addVertex(1);
+dg.addVertex(2);
+dg.addVertex(3);
+dg.addEdge(0, 1);
+dg.addEdge(1, 2);
+dg.addEdge(2, 0);
+dg.addEdge(2, 3);
 
-// Example usage:
-const g = new Graph(4);
-g.addVertex(0);
-g.addVertex(1);
-g.addVertex(2);
-g.addVertex(3);
-g.addEdge(0, 1);
-g.addEdge(1, 2);
-g.addEdge(2, 0);
-g.addEdge(2, 3);
-
-if (g.detectCycle()) {
-  console.log("Graph contains cycle");
+if (dg.detectCycle()) {
+  console.log("Directed graph contains cycle");
 } else {
-  console.log("Graph does not contain cycle");
+  console.log("Directed graph does not contain cycle");
 }
